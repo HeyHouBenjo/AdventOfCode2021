34d9cf0260a953714fa3bb5a16b6caa5eae02d4f
Benjo <benjamin.kraft@online.de>
2021-12-06 10:27:03 +0100

    day 6

--- 06/06.py
+++ 06/06.py
@@ -2,32 +2,50 @@ import numpy as np
 
 
 def readFish():
-	with open("testInput") as file:
+	with open("input") as file:
 		return np.array(file.read().split(","), dtype=np.uint8)
 
 
-def getCount(fish, days):
-	return 0
+def getCount(fish, daysLeft, usedList, usedMap):
+	if daysLeft <= fish:
+		return 1
+
+	if usedList[fish][daysLeft]:
+		return usedMap[(fish, daysLeft)]
+
+	count = 1
+	for d in range(0, daysLeft - fish, 7):
+		count += getCount(9, daysLeft - fish - d, usedList, usedMap)
+
+	usedList[fish][daysLeft] = True
+	usedMap[(fish, daysLeft)] = count
+
+	return count
 
 
 def solve(days):
-	fish = readFish()
 
+	# need to save/read known results for performance
+	usedMap = {}
+	usedList = np.zeros((10, days + 1), bool)
+
+	fish = readFish()
 	count = 0
 	for f in fish:
-		count += getCount(f, 80)
+		count += getCount(f, days, usedList, usedMap)
 
-	for i in range(0):
-		decrease = fish > 0
-		zeros = fish == 0
-		fish[decrease] -= 1
-		fish[zeros] = 6
-		fish = np.concatenate((fish, zeros[zeros].astype(np.uint8) * 8))
-		print(i, len(fish))
+	# numpy version too slow for part 2
+	# for i in range(days):
+	# 	decrease = fish > 0
+	# 	zeros = fish == 0
+	# 	fish[decrease] -= 1
+	# 	fish[zeros] = 6
+	# 	fish = np.concatenate((fish, zeros[zeros].astype(np.uint8) * 8))
+	# 	print(i, len(fish))
 
-	return len(fish)
+	return count
 
 
-# print(solve(80))
-import cProfile
-cProfile.run("print(solve(200))")
+print(solve(80))
+print(solve(256))
+
